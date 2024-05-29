0bb36fc2a25dfc4456a5327416ab89537e1b352a
karlrobeck <119088381+karlrobeck@users.noreply.github.com>
2024-05-29 21:09:19 +0800

    initial commit

---
+++ app/src/main/java/com/example/gachamobileapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.gachamobileapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.gachamobileapp.ui.theme.GachaMobileAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GachaMobileAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Gacha App")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column (
+        modifier = Modifier.
+        fillMaxWidth().
+        fillMaxHeight().
+        padding(all=12.dp).
+        border(width=2.dp,color = Color.White),
+    ) {
+        Text(
+            modifier = Modifier.padding(all=12.dp),
+            text = "Hello my name is $name"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GachaMobileAppTheme {
+        Greeting("Android")
+    }
+}
